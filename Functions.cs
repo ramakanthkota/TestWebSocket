47cb10fb8bfe07bf249e659e39ffd38ebe4957bb
Ramakanth <Ramakanth@CESIT-HF-D-004>
2020-09-16 13:14:45 +0530

    Add project files.

---
+++ Functions.cs
@@ -0,0 +1,158 @@
+using Amazon.ApiGatewayManagementApi;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace TestAWSWebSocketApi
+{
+    public class Functions
+    {
+        public const string ConnectionIdField = "connectionId";
+
+        /// <summary>
+        /// DynamoDB table used to store the open connection ids. More advanced use cases could store logged on user map to their connection id to implement direct message chatting.
+        /// </summary>
+        private string ConnectionMappingTable { get; }
+
+        /// <summary>
+        /// Factory func to create the AmazonApiGatewayManagementApiClient. This is needed to created per endpoint of the a connection. It is a factory to make it easy for tests
+        /// to moq the creation.
+        /// </summary>
+        private Func<string, IAmazonApiGatewayManagementApi> ApiGatewayManagementApiClientFactory { get; }
+
+        /// <summary>
+        /// Default constructor that Lambda will invoke.
+        /// </summary>
+        public Functions()
+        {
+            this.ApiGatewayManagementApiClientFactory = (Func<string, AmazonApiGatewayManagementApiClient>)((endpoint) =>
+            {
+                return new AmazonApiGatewayManagementApiClient(new AmazonApiGatewayManagementApiConfig
+                {
+                    ServiceURL = endpoint
+                });
+            });
+        }
+
+        /// <summary>
+        /// Constructor used for testing allow tests to pass in moq versions of the service clients.
+        /// </summary>
+        /// <param name="ddbClient"></param>
+        /// <param name="apiGatewayManagementApiClientFactory"></param>
+        /// <param name="connectionMappingTable"></param>
+        public Functions(Func<string, IAmazonApiGatewayManagementApi> apiGatewayManagementApiClientFactory, string connectionMappingTable)
+        {
+            this.ApiGatewayManagementApiClientFactory = apiGatewayManagementApiClientFactory;
+            this.ConnectionMappingTable = connectionMappingTable;
+        }
+
+        public async Task<APIGatewayProxyResponse> OnConnectHandler(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            try
+            {
+                var connectionId = request.RequestContext.ConnectionId;
+                context.Logger.LogLine($"ConnectionId: {connectionId}");
+
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = 200,
+                    Body = "Connected."
+                };
+            }
+            catch (Exception e)
+            {
+                context.Logger.LogLine("Error connecting: " + e.Message);
+                context.Logger.LogLine(e.StackTrace);
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = 500,
+                    Body = $"Failed to connect: {e.Message}"
+                };
+            }
+        }
+        //Test 12
+        public async Task<APIGatewayProxyResponse> SendMessageHandler(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            try
+            {
+                // Construct the API Gateway endpoint that incoming message will be broadcasted to.
+                var domainName = request.RequestContext.DomainName;
+                var stage = request.RequestContext.Stage;
+                var endpoint = $"https://{domainName}/{stage}";
+                context.Logger.LogLine($"API Gateway management endpoint: {endpoint}");
+
+                // The body will look something like this: {"message":"sendmessage", "data":"What are you doing?"}
+                JsonDocument message = JsonDocument.Parse(request.Body);
+
+                // Grab the data from the JSON body which is the message to broadcasted.
+                JsonElement dataProperty;
+                if (!message.RootElement.TryGetProperty("data", out dataProperty))
+                {
+                    context.Logger.LogLine("Failed to find data element in JSON document");
+                    return new APIGatewayProxyResponse
+                    {
+                        StatusCode = (int)HttpStatusCode.BadRequest
+                    };
+                }
+
+                var data = dataProperty.GetString();
+                var stream = new MemoryStream(UTF8Encoding.UTF8.GetBytes(data));
+
+                // Construct the IAmazonApiGatewayManagementApi which will be used to send the message to.
+                var apiClient = ApiGatewayManagementApiClientFactory(endpoint);
+
+                // Loop through all of the connections and broadcast the message out to the connections.
+                var count = 0;
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = (int)HttpStatusCode.OK,
+                    Body = "Data sent to " + count + " connection" + (count == 1 ? "" : "s")
+                };
+            }
+            catch (Exception e)
+            {
+                context.Logger.LogLine("Error disconnecting: " + e.Message);
+                context.Logger.LogLine(e.StackTrace);
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError,
+                    Body = $"Failed to send message: {e.Message}"
+                };
+            }
+        }
+
+        public async Task<APIGatewayProxyResponse> OnDisconnectHandler(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            try
+            {
+                var connectionId = request.RequestContext.ConnectionId;
+                context.Logger.LogLine($"ConnectionId: {connectionId}");
+
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = 200,
+                    Body = "Disconnected."
+                };
+            }
+            catch (Exception e)
+            {
+                context.Logger.LogLine("Error disconnecting: " + e.Message);
+                context.Logger.LogLine(e.StackTrace);
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = 500,
+                    Body = $"Failed to disconnect: {e.Message}"
+                };
+            }
+        }
+    }
+}
