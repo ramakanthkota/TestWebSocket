d641285148b9f192e89ceea7a37915215c6f7657
Ramakanth <Ramakanth@CESIT-HF-D-004>
2020-10-15 16:33:33 +0530

    updated

--- Functions.cs
+++ Functions.cs
@@ -258,5 +258,55 @@ namespace TestAWSWebSocketApi
                 };
             }
         }
+
+        public async Task<APIGatewayProxyResponse> OnMatchHandler(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            try
+            {
+                // Construct the API Gateway endpoint that incoming message will be broadcasted to.12
+                var domainName = request.RequestContext.DomainName;
+                var stage = request.RequestContext.Stage;
+                var endpoint = $"https://{domainName}/{stage}";
+                context.Logger.LogLine($"API Gateway management endpoint: {endpoint}");
+
+                // The body will look something like this: {"message":"blockuser", "data":"What are you doing?"}
+                JsonDocument message = JsonDocument.Parse(request.Body);
+
+                // Grab the data from the JSON body which is the message to broadcasted.
+                JsonElement dataProperty;
+                if (!message.RootElement.TryGetProperty("data", out dataProperty))
+                {
+                    context.Logger.LogLine("Failed to find data element in JSON document");
+                    return new APIGatewayProxyResponse
+                    {
+                        StatusCode = (int)HttpStatusCode.BadRequest
+                    };
+                }
+
+                var data = dataProperty.GetString();
+                var stream = new MemoryStream(UTF8Encoding.UTF8.GetBytes(data));
+
+                // Construct the IAmazonApiGatewayManagementApi which will be used to send the message to.
+                var apiClient = ApiGatewayManagementApiClientFactory(endpoint);
+
+                // Loop through all of the connections and broadcast the message out to the connections. test
+                var count = 0;
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = (int)HttpStatusCode.OK,
+                    Body = "Data sent to " + count + " connection" + (count == 1 ? "" : "s")
+                };
+            }
+            catch (Exception e)
+            {
+                context.Logger.LogLine("Error disconnecting: " + e.Message);
+                context.Logger.LogLine(e.StackTrace);
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError,
+                    Body = $"Failed to send message: {e.Message}"
+                };
+            }
+        }
     }
 }
